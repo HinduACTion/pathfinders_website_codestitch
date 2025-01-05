bb3fab1094d6c83a08a333c0d132008a1fad5854
HinduACTion <hinduaction@proton.me>
2025-01-05 17:21:05 +0000

    changes to contact

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,21 @@
 module.exports = {
-    name: "Code Stitch Web Designs",
-    email: "help@codestitch.app",
-    phoneForTel: "555-779-4407",
-    phoneFormatted: "(555) 779-4407",
+    name: "HinduACTion",
+    email: "hinduaction@gmail.com",
+    phoneForTel: "202-838-6655",
+    phoneFormatted: "(202) 838-6655",
     address: {
-        lineOne: "First Address Line",
-        lineTwo: "Second Address Line",
-        city: "Denver",
-        state: "CO",
-        zip: "80206",
+        lineOne: "HinduACTion",
+        lineTwo: "PO Box: 882",
+        city: "West Chester",
+        state: "OH",
+        zip: "45071",
         country: "US",
-        mapLink: "https://goo.gl/maps/UAQn4vuGDiwv7DV39",
+        // mapLink: "https://goo.gl/maps/UAQn4vuGDiwv7DV39",
     },
-    socials: {
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
+    // socials: {
+    //     facebook: "https://www.facebook.com/",
+    //     instagram: "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.example.com",
+    domain: "https://www.hindu-action.org/",
 };
