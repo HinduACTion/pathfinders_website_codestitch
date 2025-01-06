dfa5221c2a236f95db9ab3d4a5e0edb311fcd6ca
HinduACTion <hinduaction@proton.me>
2025-01-06 03:01:52 +0000

    fixed client.js

--- src/_data/client.js
+++ src/_data/client.js
@@ -15,7 +15,7 @@ module.exports = {
     // socials: {
     //     facebook: "https://www.facebook.com/",
     //     instagram: "https://www.instagram.com/",
-    },
+    // },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.hindu-action.org/",
+    domain: "https://americanhindupathfinders.com" ,
 };
